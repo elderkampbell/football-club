ffdfc1be8722bfa4ed9a091cfe6d1587348df954
elderkampbell <elderkampbell@hotmail.com>
2023-03-09 08:49:29 -0300

    fix: users model

--- app/backend/src/database/models/UsersModel.ts
+++ app/backend/src/database/models/UsersModel.ts
@@ -4,7 +4,10 @@ import db from '.';
 
 class Users extends Model {
   declare id: number;
-  declare teamName: string;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
 }
 
 Users.init({
