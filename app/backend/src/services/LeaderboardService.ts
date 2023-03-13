8a7685e457c1aaa90b7de6f8f377d090c7c40d5b
elderkampbell <elderkampbell@hotmail.com>
2023-03-13 12:02:50 -0300

    refactor: SQL query

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -5,21 +5,22 @@ import Matches from '../database/models/MatchesModel';
 export default class LeaderboardService implements ILeaderboardService {
   protected model: ModelStatic<Matches> = Matches;
   async getAll(): Promise<object[] | undefined> {
-    const query = `SELECT
-  t.team_name AS name,
-  CAST(SUM(IF(m.home_team_goals > m.away_team_goals, 3, 0) +
-  IF(m.home_team_goals = m.away_team_goals, 1, 0)) AS SIGNED) AS totalPoints,
-  COUNT(m.home_team_id) AS totalGames,
-  CAST(SUM(IF(m.home_team_goals > m.away_team_goals, 1, 0)) AS SIGNED) AS totalVictories,
-  CAST(SUM(IF(m.home_team_goals = m.away_team_goals, 1, 0)) AS SIGNED) AS totalDraws,
-  CAST(SUM(IF(m.home_team_goals < m.away_team_goals, 1, 0)) AS SIGNED) AS totalLosses,
-  CAST(SUM(m.home_team_goals) AS SIGNED) AS goalsFavor,
-  CAST(SUM(m.away_team_goals) AS SIGNED) AS goalsOwn
-  FROM TRYBE_FUTEBOL_CLUBE.matches m
-  LEFT JOIN TRYBE_FUTEBOL_CLUBE.teams t
-  ON t.id = m.home_team_id
-  WHERE m.in_progress = false
-  GROUP BY t.team_name;`;
+    const totalPoints = `SUM(CASE WHEN m.home_team_goals > m.away_team_goals THEN 3
+      WHEN m.home_team_goals = m.away_team_goals THEN 1 ELSE 0 END)`;
+    const query = `SELECT 
+    t.team_name AS name, CAST(${totalPoints} AS SIGNED) AS totalPoints, COUNT(*) AS totalGames,
+    SUM(CASE WHEN m.home_team_goals > m.away_team_goals THEN 1 ELSE 0 END) AS totalVictories,
+    SUM(CASE WHEN m.home_team_goals = m.away_team_goals THEN 1 ELSE 0 END) AS totalDraws,
+    SUM(CASE WHEN m.home_team_goals < m.away_team_goals THEN 1 ELSE 0 END) AS totalLosses,
+    SUM(m.home_team_goals) AS goalsFavor,
+    SUM(m.away_team_goals) AS goalsOwn,
+    SUM(m.home_team_goals - m.away_team_goals) AS goalsBalance,
+    CAST(${totalPoints} / (COUNT(*) * 3) * 100 AS DECIMAL(6,2)) AS efficiency
+    FROM TRYBE_FUTEBOL_CLUBE.matches AS m 
+    LEFT JOIN TRYBE_FUTEBOL_CLUBE.teams AS t ON t.id = m.home_team_id 
+    WHERE m.in_progress = FALSE
+    GROUP BY t.team_name 
+ORDER BY totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC, goalsOwn DESC;`;
     const result = await this.model.sequelize?.query(query, { type: QueryTypes.SELECT });
     return result;
   }
